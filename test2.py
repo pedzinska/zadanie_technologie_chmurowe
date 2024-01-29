79771618e1f8d096163f98aa2f67db07b957fb55
Anna <anna.pedzinska@edu.uekat.pl>
2024-01-29 22:19:16 +0100

    rozwiazanie 29.01

---
+++ test2.py
@@ -0,0 +1,53 @@
+import cv2
+from flask import Flask, request
+from flask_restful import Resource, Api
+from urllib.request import urlretrieve
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+app = Flask(__name__)
+api = Api(app)
+
+
+class PeopleCounterStatic(Resource):
+    def get(self):
+        # load image
+        image = cv2.imread('Family-at-the-beach.jpg')
+        image = cv2.resize(image, (700, 400))
+
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'peopleCount': len(rects)}
+
+
+class PeopleCounterDynamicUrl(Resource):
+    def get(self):
+        url = 'https://img.freepik.com/free-photo/people-surfing-brazil_23-2151079355.jpg?w=996&t=st=1706561159~exp=1706561759~hmac=60f4370805a82af086129a965e83740cc569753d7c75ff6879c9d72070b0204c'
+        img_path, _ = urlretrieve(url, "downloaded_image.jpg")
+        image2 = cv2.imread(img_path)
+        #image2 = cv2.imread(urlretrieve('https://img.freepik.com/free-photo/people-surfing-brazil_23-2151079355.jpg?w=996&t=st=1706561159~exp=1706561759~hmac=60f4370805a82af086129a965e83740cc569753d7c75ff6879c9d72070b0204c', "ludzie"))
+        # TODO:
+        # 1. Pobrać zdjęcie z otrzymanego adresu
+        # 2. Pobrane zdjęcie można zapisać na dysku lub przetwarzać je w pamięci podręcznej
+        # 3. Załadowane zjęcie do zmiennej image przekazać do algorytmu hog.detectMultiScale i zwrócić z endpointu liczbę wykrytych osób.
+
+        image2 = cv2.resize(image2, (700, 400))
+
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image2, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+        return {'peopleCount': len(rects)}
+
+        #url = request.args.get('url')
+        #print('url', url)
+        #return {'peopleCount': 0}
+
+
+api.add_resource(PeopleCounterStatic, '/')
+api.add_resource(PeopleCounterDynamicUrl, '/dynamic')
+
+if __name__ == '__main__':
+    app.run(debug=True)
