39ce573843344bf613696d9ab9cd796ed19d2306
Anna <anna.pedzinska@edu.uekat.pl>
2024-01-31 19:26:18 +0100

    rozwiazanie 31.01

--- main.py
+++ main.py
@@ -20,8 +20,6 @@ class PeopleCounterStatic(Resource):
         (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
 
         return {'peopleCount': len(rects)}
-
-
 class PeopleCounterDynamicUrl(Resource):
     def get(self):
         url = 'https://img.freepik.com/free-photo/people-surfing-brazil_23-2151079355.jpg?w=996&t=st=1706561159~exp=1706561759~hmac=60f4370805a82af086129a965e83740cc569753d7c75ff6879c9d72070b0204c'
