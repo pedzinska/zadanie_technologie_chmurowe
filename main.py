c9c8ee7a10b548cbb2f6fdd4d27fd15eb2f015f5
Anna <anna.pedzinska@edu.uekat.pl>
2024-01-31 19:46:49 +0100

    rozwiazanie 31.01

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class PeopleCounterStatic(Resource):
 
         return {'peopleCount': len(rects)}
 class PeopleCounterDynamicUrl(Resource):
+
     def get(self):
         url = 'https://img.freepik.com/free-photo/people-surfing-brazil_23-2151079355.jpg?w=996&t=st=1706561159~exp=1706561759~hmac=60f4370805a82af086129a965e83740cc569753d7c75ff6879c9d72070b0204c'
         img_path, _ = urlretrieve(url, "downloaded_image.jpg")
